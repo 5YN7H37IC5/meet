2991113edbe9057c6109d671ff8909156305aefb
TheMindExpansionNetwork <wethepeople541@outlook.com>
2025-05-21 16:11:06 -0700

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ function DemoMeetingTab(props: { label: string }) {
   };
   return (
     <div className={styles.tabContent}>
-      <p style={{ margin: 0 }}>Try LiveKit Meet for free with our live demo project.</p>
+      <p style={{ margin: 0 }}>Talk to  Zara Moon</p>
       <button style={{ marginTop: '1rem' }} className="lk-button" onClick={startMeeting}>
         Start Meeting
       </button>
@@ -106,7 +106,7 @@ function CustomConnectionTab(props: { label: string }) {
   return (
     <form className={styles.tabContent} onSubmit={onSubmit}>
       <p style={{ marginTop: 0 }}>
-        Connect LiveKit Meet with a custom server using LiveKit Cloud or LiveKit Server.
+        Connect with Zara Moon she is only one click away.
       </p>
       <input
         id="serverUrl"
